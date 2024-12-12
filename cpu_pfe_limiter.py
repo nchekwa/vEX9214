683e484a15074cbe0e0fc411b671e34d3ddec4b0
Artur Zdolinski <artur@zdolinski.com>
2024-12-12 04:39:19 +0100

    Update cpu_pfe_limiter.py
    add cmdline_2 / 2nd identifier

--- cpu_pfe_limiter.py
+++ cpu_pfe_limiter.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 # Created by: Artur Zdolinski
-# Add to cronetab
-# echo "*/2 *   * * *   root    python3 /root/cpu_pfe_limiter.py" >> /etc/crontab
+#
+# Crontab:
+# echo "*/2 *   * * *   root    /usr/bin/python3 /root/cpu_pfe_limiter.py" >> /etc/crontab
 
-import psutil                # ubuntu: apt-get install python3-psutil
+import psutil   # ubuntu: apt-get install python3-psutil
 import subprocess
 
-search_in_cmdline = "-PFE"  # Target process identifier | grep to search on ps ax correct identifier which will match your setup (ie. name QEMU running instance with Leaf-PFE)
-cpu_limit_percent = 25  # CPU limit percentage
+search_in_cmdline_1 = "-PFE"           # Target process identifier 1
+search_in_cmdline_2 = "qemu-system"    # Target process identifier 2
+cpu_limit_percent = 25                 # CPU limit percentage
 
 for proc in psutil.process_iter(['pid', 'name', 'cmdline']):
     try:
-        if proc.info and search_in_cmdline in ' '.join(proc.info['cmdline']):
+        cmdline = ' '.join(proc.info['cmdline'])
+        if proc.info and search_in_cmdline_1 in cmdline and search_in_cmdline_2 in cmdline:  # Check both conditions
             net24_pid = proc.info['pid']
             cpulimit_running = False
             for p in psutil.process_iter(['pid', 'name', 'cmdline']):
@@ -22,14 +25,14 @@ for proc in psutil.process_iter(['pid', 'name', 'cmdline']):
                         break
                 except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                     pass
+
             if not cpulimit_running:
-                cmdline = ' '.join(proc.info['cmdline'])
                 name_index = cmdline.find('-name')
                 if name_index != -1:
                     name = cmdline[name_index + 6:].split()[0]
                     print(f"PID: {net24_pid}, Nazwa: {name}")
-
                 subprocess.Popen(f"cpulimit -p {net24_pid} -l {cpu_limit_percent} -b", shell=True)
 
     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
         pass
+
